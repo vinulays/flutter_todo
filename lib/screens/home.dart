8d309463056a848b9e06e3b7483bebb67e89e9aa
Vinula Senarathne <vinulapc@gmail.com>
2023-11-28 18:43:05 +0530

    feat: todo add input field and button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,28 +12,75 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 50, bottom: 20),
-                    child: const Text(
-                      "All ToDos",
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
-                    ),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 50, bottom: 20),
+                        child: const Text(
+                          "All ToDos",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      for (ToDo todoo in todosList) ToDoItem(todo: todoo),
+                    ],
                   ),
-                  for (ToDo todoo in todosList) ToDoItem(todo: todoo),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 1.0,
+                            spreadRadius: 0.0)
+                      ],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        hintText: "Add a new todo item",
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 20, right: 20),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    minimumSize: const Size(60, 60),
+                    elevation: 1,
+                  ),
+                  child: const Text(
+                    '+',
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+              )
+            ]),
+          )
+        ],
       ),
     );
   }
